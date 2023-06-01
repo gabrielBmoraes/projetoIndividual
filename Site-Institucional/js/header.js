79a9725a9ceef3fe2876266226bd3159e4e13461
gabrielBmoraes <gabriel.moraes@sptech.school>
2023-06-01 08:19:48 -0300

    2.1

--- Site-Institucional/js/header.js
+++ Site-Institucional/js/header.js
@@ -1,3 +1,9 @@
-if (listaSobre.value == '1'){
-    window.location.href = 'https://www.youtube.com/'
-}        
+function mudarSobre() {
+    alert("entrou na função")
+    if (listaSobre.value == 1) {
+        window.location.href = "C:\Users\Gabriel\Desktop\projetoIndividual\Site-Institucional\html\ipb.html"
+    }
+    if (listaSobre.value == 2) {
+        window.location.href = "C:\Users\Gabriel\Desktop\projetoIndividual\Site-Institucional\html\Upa.html"
+    }
+}
